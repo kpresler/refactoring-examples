586856c38d1d3b0b2b0f0a0aa5e7a081ea96ed13
Kai Presler-Marshall <kpresle@ncsu.edu>
2022-11-09 09:07:19 -0500

    Remove spare methods

--- src/Student.java
+++ src/Student.java
@@ -21,38 +21,6 @@ public class Student {
 		this.classStanding = classStanding;
 	}
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getDob() {
-		return dob;
-	}
-
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-
-	public String getClassStanding() {
-		return classStanding;
-	}
-
-	public void setClassStanding(String classStanding) {
-		this.classStanding = classStanding;
-	}
-
 	@Override
 	public String toString() {
 		return "Student [lastName=" + lastName + ", firstName=" + firstName + ", dob=" + dob + ", classStanding="
